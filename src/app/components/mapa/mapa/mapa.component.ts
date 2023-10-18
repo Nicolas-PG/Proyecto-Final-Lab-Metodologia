2fd0658ddecc1fb5fb9b35c27fc839b2057ebdfc
Nicolas Pulti <nicopultigisondo@gmail.com>
2023-10-18 18:48:11 -0300

    Se agrego la la funcion de encontrar la ubicacion actual

--- src/app/components/mapa/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa/mapa.component.ts
@@ -9,10 +9,9 @@ import { GoogleMapsService } from 'src/app/services/google-maps.service';
 export class MapaComponent implements OnInit {
 
   private mapa?: google.maps.Map;
+  private botonUbicacion?: HTMLButtonElement;
 
-  constructor(private googleMapsService:GoogleMapsService){
-    
-  }
+  constructor(private googleMapsService:GoogleMapsService){ }
 
   ngOnInit(): void {
     
@@ -20,10 +19,18 @@ export class MapaComponent implements OnInit {
 
     this.googleMapsService.cargarMapa(mapElement as HTMLElement, {
       center:{lat:-38.005614285436465, lng:-57.54347888235542}, 
-      zoom:13
+      zoom:13,
+      styles: [
+        {
+          featureType: "poi",
+          elementType: "labels",
+          stylers: [{ visibility: "off" }],
+        },
+      ],
     
     }).then(mapa => {
       this.mapa = mapa;
+      this.botonUbicacion = this.googleMapsService.botonGeolocalizacion(mapa);
     });
 
   }
--- src/app/services/google-maps.service.ts
+++ src/app/services/google-maps.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
+import { map } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 
 @Injectable({
@@ -24,14 +25,65 @@ export class GoogleMapsService {
     return this.loader.importLibrary('maps').then(() =>{
       return new google.maps.Map(element,opciones);  // esta funcion luego se llama en mapa coponent y se le agrega los datos
     });
-   }
+  }
 
+
+
+  // ESTA FUNCION CREA UN BOTON QUE DESPLIEGA UN INFOWINDOW PARA PERMITIR ENCONTRAR LA UBICACION ACTUAL Y POSICIONCARSE EN EL MAPA
+
+    botonGeolocalizacion(map: google.maps.Map): HTMLButtonElement{
+      const locationButton = document.createElement("button");
+      locationButton.textContent = "Deslplazarse a la ubicación actual";
+      locationButton.classList.add("btn-map-location");
+
+      map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
+      locationButton.addEventListener("click", () => {
+      
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position: GeolocationPosition) => {
+              const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              };
+    
+              const infoWindow = new google.maps.InfoWindow;
+              infoWindow.setPosition(pos);
+              infoWindow.setContent("Ubicacion encontrada.");
+              infoWindow.open(map);
+              map.setCenter(pos);
+            },
+            () => {
+              this.handleLocationError(true, map);
+            }
+          );
+        } else {
+          
+          this.handleLocationError(false, map);
+        }
+      });
+
+      return locationButton;
+    }
+
+    // ESTA FUNCION MANEJA LOS CASOS DE ERRORES DE LA FUNCION DE GEOLOCALIZACION
+    private handleLocationError(browserHasGeolocation: boolean, map:google.maps.Map):void {
+
+      const infoWindow = new google.maps.InfoWindow();
+      infoWindow.setPosition(map.getCenter()!);
+
+      infoWindow.setContent(
+        browserHasGeolocation
+        ?"El servicio de geolocalizacion fallo"
+        :"El buscador no soporta geolocalizacion"
+      );
+        infoWindow.open(map);
+
+    }
+    
+   
 
    
 
-
-
-
-   
-}
-
+  }
