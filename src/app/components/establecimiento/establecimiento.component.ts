a7d42f041abe9e5c799a4b7bb5af26d9af034998
Luca Castelao <luca.castelao@gmail.com>
2023-11-02 13:40:55 -0300

    diseño de apartado Lugares

--- src/app/components/establecimiento/establecimiento.component.ts
+++ src/app/components/establecimiento/establecimiento.component.ts
@@ -11,6 +11,14 @@ export class EstablecimientoComponent implements OnInit {
 
    establecimientos: any[] = [];
 
+   establecimientosPruebaFront: any[] = [
+    {nombre:'Banderita', direccion: 'Guemes 2000', img:'https://banderitaparrilla.com/wp-content/uploads/2021/12/DSC_4561.jpg'},
+    {nombre:'Antares', direccion: 'Olavarria 2000', img:'https://media-cdn.tripadvisor.com/media/photo-s/19/8e/4e/f9/frente-local-antares.jpg'},
+    {nombre:'Cafe Martinez', direccion: 'Cordoba 2000', img:'https://www.equipamientoyservicios.com.ar/wp-content/uploads/2021/06/CAFE-MARTINEZ-OLIVOS-DSC_0613-800-X-510.jpg'},
+    {nombre:'Borneo', direccion: 'Moreno 2000', img:'https://www.borneobrewingco.com.ar/wp-content/uploads/2022/10/Nurien-00680.jpg'},
+    {nombre:'Parliamo', direccion: 'Rivadavia 2000', img:'https://parliamopizza.com/wp-content/uploads/2021/09/WhatsApp-Image-2021-09-27-at-10.56.15-1.jpeg'}
+   ]
+
   constructor(private establecimientoService : EstablecimientoService){}
 
   ngOnInit(): void {
