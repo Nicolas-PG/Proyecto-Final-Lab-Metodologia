81fbb00adc5a2ae2bf3fbb7d278b584a8b316a8c
ezequiel <ezequieldellasanta@hotmail.com.ar>
2023-11-05 23:24:36 -0300

    Se agrego una funcion para dejar comentario si el usario esta registrado

--- src/app/components/comentarios/comentarios.component.ts
+++ src/app/components/comentarios/comentarios.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-comentarios',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./comentarios.component.css']
 })
 export class ComentariosComponent {
+  comments: string[] = [];
+  newComment: string = '';
+ 
+
+  isLoggedIn: boolean;
+  mostrarFiltro: boolean = false;
+
+  addComment() {
+    if (this.newComment) {
+      this.comments.push(this.newComment);
+      this.newComment = ''; 
+    }
+  }
+
+
+  constructor(private authService: AuthService) {
+
+    this.isLoggedIn = authService.isLoggedIn;
+  }
+
+
+
+
+
+
 
 }
